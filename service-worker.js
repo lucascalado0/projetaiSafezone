93b755d90a8f5327eb098d3e7bd40e5f9dcaf7a9
devlucascalado <devlucascalado@gmail.com>
2024-06-01 15:15:11 -0300

    service-worker

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+var cacheName = 'SafeZone'
+
+self.addEventListener('install', event =>{
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll([
+                'index.html',
+                'formRelato.html',
+                'ocorrencias.html',
+                
+                './assets/css/main.css',
+                './assets/js/map.js',
+
+
+                './assets/image/icon_lista.png',
+                './assets/image/icon_pindrop.png',
+                './assets/image/iconpin.png',
+                './assets/image/icon_mapa'
+            ]))
+    );
+});
+
+
+self.addEventListener('message', function(event){
+    if(event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
+
+self.addEventListener('fetch', function (event){
+    event.repondWith(async function(){
+        try {
+            return await fetch(event.request);
+        } catch (err){
+            return caches.match(event.request);
+        }   
+    }());
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if(response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+
+
+
+
